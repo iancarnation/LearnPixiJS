2eebbae9306dd29f1b125c5c76253b5fba067b1b
Ian Rich <iancarnation@gmail.com>
2017-04-19 17:13:58 -0700

    added notion of game state

--- learn.js
+++ learn.js
@@ -1,8 +1,3 @@
-function randomInt(min, max)
-{
-    return Math.floor(Math.random() * (max-min+1)) + min;
-}
-
 var stage = new PIXI.Container(),
     renderer = PIXI.autoDetectRenderer(512, 512);
 document.body.appendChild(renderer.view);
@@ -12,30 +7,43 @@ PIXI.loader
     .add("img/cat.png")
     .load(setup);
 
-var cat;
+var cat, state;
     
 function setup()
 {
     cat = new PIXI.Sprite(PIXI.loader.resources["img/cat.png"].texture);
     cat.y = 96;
-    
     cat.vx = 0;
     cat.vy = 0;
-    
     stage.addChild(cat);
+
+    state = play;
     
     gameLoop();
 }
 
+
 function gameLoop()
 {
     requestAnimationFrame(gameLoop);
-    
-    cat.vx = 1;
-    cat.vy = 1;
-    
-    cat.x += cat.vx;
-    cat.y += cat.vy;
+    state();
     
     renderer.render(stage);
+}
+
+function play()
+{
+    cat.vx = 1;
+    cat.x += cat.vx;
+}
+
+
+function keyboard(KeyCode)
+{
+    
+}
+
+function randomInt(min, max)
+{
+    return Math.floor(Math.random() * (max-min+1)) + min;
 }
