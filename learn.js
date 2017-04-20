2cf123d9148de03def918c7c403014aeb8fabf47
Ian Rich <iancarnation@gmail.com>
2017-04-19 17:30:17 -0700

    keyboard controls

--- learn.js
+++ learn.js
@@ -16,7 +16,49 @@ function setup()
     cat.vx = 0;
     cat.vy = 0;
     stage.addChild(cat);
-
+    
+    var left = keyboard(37),
+        up = keyboard(38),
+        right = keyboard(39),
+        down = keyboard(40);
+        
+    left.press = function(){
+        cat.vx = -5;
+        cat.vy = 0;
+    };
+    left.release = function(){
+        if (!right.isDown && cat.vy === 0)
+            cat.vx = 0;
+    };
+    
+    up.press = function(){
+        cat.vx = 0;
+        cat.vy = -5;
+    };
+    up.release = function(){
+        if (!down.isDown && cat.vx === 0)
+            cat.vy = 0;
+    };
+    
+    right.press = function(){
+        cat.vx = 5;
+        cat.vy = 0;
+    };
+    right.release = function(){
+        if (!left.isDown && cat.vy === 0)
+            cat.vx = 0;
+    };
+    
+    down.press = function(){
+        cat.vx = 0;
+        cat.vy = 5;
+    };
+    down.release = function(){
+        if (!up.isDown && cat.vx === 0)
+            cat.vy = 0;
+    };
+    
+    
     state = play;
     
     gameLoop();
@@ -33,14 +75,44 @@ function gameLoop()
 
 function play()
 {
-    cat.vx = 1;
     cat.x += cat.vx;
+    cat.y += cat.vy;
 }
 
 
-function keyboard(KeyCode)
+function keyboard(keyCode)
 {
+    var key = {};
+    key.code = keyCode;
+    key.isDown = false;
+    key.isUp = true;
+    key.press = undefined;
+    key.release = undefined;
     
+    key.onKeyDown = function(event){
+        if (event.keyCode === key.code)
+        {
+            if (key.isUp && key.press) key.press();
+            key.isDown = true;
+            key.isUp = false;
+        }
+        event.preventDefault();
+    };
+    
+    key.onKeyUp = function(event){
+        if (event.keyCode === key.code)
+        {
+            if (key.isDown && key.release) key.release();
+            key.isDown = false;
+            key.isUp = true;
+        }
+        event.preventDefault();
+    };
+    
+    window.addEventListener("keydown", key.onKeyDown.bind(key), false);
+    window.addEventListener("keyup", key.onKeyUp.bind(key), false);
+    
+    return key;
 }
 
 function randomInt(min, max)
